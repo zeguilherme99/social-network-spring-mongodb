e1b8786d932757725cb285e0e9d59aa191c83cf2
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-02 00:44:56 -0300

    Changing Long to String

--- src/main/java/com/learning/socialnetwork/repositories/UserRepository.java
+++ src/main/java/com/learning/socialnetwork/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package com.learning.socialnetwork.repositories;
 import com.learning.socialnetwork.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, Long>{
+public interface UserRepository extends MongoRepository<User, String>{
 
 }

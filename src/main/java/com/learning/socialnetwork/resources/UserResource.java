599d5ae9c8ac7f635e239ed988729a41da47f36c
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-02 02:16:34 -0300

    User delete

--- src/main/java/com/learning/socialnetwork/resources/UserResource.java
+++ src/main/java/com/learning/socialnetwork/resources/UserResource.java
@@ -5,8 +5,11 @@ import com.learning.socialnetwork.entities.User;
 import com.learning.socialnetwork.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +31,18 @@ public class UserResource {
     public UserDTO findById(@PathVariable String id) {
         User obj = service.findById(id);
         return new UserDTO(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable String id) {
+        service.delete(id);
     }
 }
--- src/main/java/com/learning/socialnetwork/services/UserService.java
+++ src/main/java/com/learning/socialnetwork/services/UserService.java
@@ -1,5 +1,6 @@
 package com.learning.socialnetwork.services;
 
+import com.learning.socialnetwork.dto.UserDTO;
 import com.learning.socialnetwork.entities.User;
 import com.learning.socialnetwork.repositories.UserRepository;
 import com.learning.socialnetwork.services.exception.ObjectNotFoundException;
@@ -22,5 +23,17 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public void insert(User obj) {
+        userRepository.save(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }

9bea30af9c57ea748e9c5b9e2885debe80c0d090
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-02 04:18:42 -0300

    Simple consults with @Query

--- src/main/java/com/learning/socialnetwork/repositories/PostRepository.java
+++ src/main/java/com/learning/socialnetwork/repositories/PostRepository.java
@@ -2,10 +2,14 @@ package com.learning.socialnetwork.repositories;
 
 import com.learning.socialnetwork.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Set;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+
+    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    Set<Post> searchTitle(String text);
 
     Set<Post> findByTitleContainingIgnoreCase(String text);
 }

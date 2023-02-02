13916987eadf428320a9232e70b1c56068e1ca6b
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-02 03:06:14 -0300

    Post Entity with aligned User

--- src/main/java/com/learning/socialnetwork/config/Instantiation.java
+++ src/main/java/com/learning/socialnetwork/config/Instantiation.java
@@ -1,11 +1,14 @@
 package com.learning.socialnetwork.config;
 
+import com.learning.socialnetwork.entities.Post;
 import com.learning.socialnetwork.entities.User;
+import com.learning.socialnetwork.repositories.PostRepository;
 import com.learning.socialnetwork.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -13,15 +16,24 @@ public class Instantiation implements CommandLineRunner {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, Instant.parse("2023-01-22T19:53:07Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, Instant.parse("2023-01-22T19:53:07Z"), "Bom dia", "Acordei feliz hoje", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/learning/socialnetwork/entities/Post.java
@@ -0,0 +1,83 @@
+package com.learning.socialnetwork.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Document
+public class Post implements Serializable {
+
+    @Id
+    public String id;
+    private Instant moment;
+    private String title;
+    private String body;
+    private User author;
+
+    public Post() {
+    }
+
+    public Post(String id, Instant moment, String title, String body, User author) {
+        this.id = id;
+        this.moment = moment;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
